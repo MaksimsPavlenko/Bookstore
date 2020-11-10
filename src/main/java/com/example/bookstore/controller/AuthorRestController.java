59a9c7737300eaa8f584273fabca45b5ce6555b6
MaksimsPavlenko <41535905+MaksimsPavlenko@users.noreply.github.com>
2020-11-11 01:38:18 +0200

    added AuthorRestControllers methods addAuthor,authorList

--- src/main/java/com/example/bookstore/controller/AuthorRestController.java
+++ src/main/java/com/example/bookstore/controller/AuthorRestController.java
@@ -1,12 +1,29 @@
 package com.example.bookstore.controller;
 
+import com.example.bookstore.model.Author;
 import com.example.bookstore.service.AuthorServices;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class AuthorRestController {
 
     @Autowired
     private AuthorServices authorServices;
+
+    @RequestMapping(value = "/authorList", method = RequestMethod.GET)
+    public List<Author> authors(){
+        return authorServices.findAllAuthors();
+    }
+
+    @RequestMapping(value = "/authorList", method = RequestMethod.GET)
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public String addAuthor(@RequestBody Author author){
+        authorServices.addAuthor(author);
+        return "";
+    }
+
 }

7618996799ab180fe7e3f8eaf03a30e1552a9b2c
MaksimsPavlenko <41535905+MaksimsPavlenko@users.noreply.github.com>
2020-11-11 14:13:29 +0200

    added bookcontroller methods addBook, bookList

--- src/main/java/com/example/bookstore/controller/BookRestController.java
+++ src/main/java/com/example/bookstore/controller/BookRestController.java
@@ -1,12 +1,27 @@
 package com.example.bookstore.controller;
 
+import com.example.bookstore.model.Book;
 import com.example.bookstore.service.BookServices;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class BookRestController {
 
     @Autowired
     private BookServices bookServices;
+
+    @RequestMapping(value = "/bookList", method = RequestMethod.GET)
+    public List<Book> books(){
+        return bookServices.findAllBooks();
+    }
+
+    @RequestMapping(value = "/addBook", method = RequestMethod.POST)
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public Book addBook(@RequestBody Book book){
+        return bookServices.addBook(book);
+    }
 }

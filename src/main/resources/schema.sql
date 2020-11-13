b3625126eeedb0d44e9c6e68a3d42eab52b42310
MaksimsPavlenko <41535905+MaksimsPavlenko@users.noreply.github.com>
2020-11-14 01:44:34 +0200

    updated sql tables deleted unused field

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -17,13 +17,12 @@
 #
 # );
 CREATE TABLE IF NOT EXISTS book
-(
-    book_id				int AUTO_INCREMENT PRIMARY KEY,
-    title			   	varchar(20) UNIQUE NOT NULL,
-    publisher			varchar(20) NOT NULL,
-    published_date		date NOT NULL ,
-    author_fk			int REFERENCES author (author_id)
-);
+  (
+      book_id			int AUTO_INCREMENT PRIMARY KEY,
+      title			   	varchar(20) UNIQUE NOT NULL,
+      publisher			varchar(20) NOT NULL,
+      published_date	date NOT NULL
+  );
 
 CREATE TABLE IF NOT EXISTS author
 (
@@ -32,3 +31,22 @@ CREATE TABLE IF NOT EXISTS author
     last_name	varchar(20) NOT NULL
 
 );
+
+
+#
+INSERT INTO book (title, publisher, published_date)
+VALUES ('Java concepts', 'java publisher', '2020-11-10');
+INSERT INTO book (title, publisher, published_date)
+VALUES ('C++ concepts', 'Maksims', '2020-11-10');
+
+INSERT INTO author (first_name, last_name, books_book_id)
+VALUES ('Maksims', 'Pavlenko', 1);
+INSERT INTO author (first_name, last_name, books_book_id)
+VALUES ('Aleksejs', 'Berzins', 1);
+
+INSERT INTO book_authors (book_book_id, authors_author_id)
+VALUES (1, 1);
+INSERT INTO book_authors (book_book_id, authors_author_id)
+VALUES (1, 2);
+INSERT INTO book_authors (book_book_id, authors_author_id)
+VALUES (2, 1)

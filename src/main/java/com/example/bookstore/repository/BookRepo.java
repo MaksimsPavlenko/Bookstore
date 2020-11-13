f94fed00c8d0791982781e7780f0e9ad23592db6
MaksimsPavlenko <41535905+MaksimsPavlenko@users.noreply.github.com>
2020-11-13 23:23:05 +0200

    added test query findByKeyWord for UI

--- src/main/java/com/example/bookstore/repository/BookRepo.java
+++ src/main/java/com/example/bookstore/repository/BookRepo.java
@@ -2,8 +2,22 @@ package com.example.bookstore.repository;
 
 import com.example.bookstore.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BookRepo extends JpaRepository<Book, Integer> {
+
+    //list of books with author  using query
+    @Query("SELECT new com.example.bookstore.model.Book(b.title, a.firstName, a.lastName) FROM Book b JOIN b.authors a")
+    public List<Book> getBookAndAuthors();
+
+    //filter for ui
+    @Query( value = "SELECT b FROM Book b WHERE b.title LIKE %:keyword%")
+    public List<Book> findByKeyWord(@Param("keyword") String keyword);
+
+
 }
